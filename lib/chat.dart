00ae45aefa0ec0e7a6de4754b43d755a0e8f6877
Arto Bendiken <arto@conreality.org>
2018-09-15 07:10:19 +0300

    Sketched out the chat screen.

--- lib/chat.dart
+++ lib/chat.dart
@@ -19,6 +19,9 @@ class ChatScreen extends StatefulWidget {
 class ChatState extends State<ChatScreen> {
   static const platform = MethodChannel('app.conreality.org/chat');
 
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,11 +31,93 @@ class ChatState extends State<ChatScreen> {
           IconButton(icon: Icon(Icons.more_vert), onPressed: _ignore),
         ],
       ),
-      body: Center(
-        child: Text('To be implemented.'), // TODO
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      )
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(
+                  hintText: "Send a message",
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   void _ignore() {}
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(text: text);
+    setState(() { _messages.insert(0, message); });
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text('I')),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Me', style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
