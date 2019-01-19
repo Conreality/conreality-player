18fe6ba993ab23c88bb2fe264bc3daea3ec4947d
Arto Bendiken <arto@conreality.org>
2019-01-19 18:35:19 +0200

    Added mock headset and heartbeat indicators.

--- lib/player_tab.dart
+++ lib/player_tab.dart
@@ -1,6 +1,7 @@
 /* This is free and unencumbered software released into the public domain. */
 
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import 'api.dart' as API;
 
@@ -19,6 +20,7 @@ class PlayerTab extends StatefulWidget {
 class PlayerState extends State<PlayerTab> {
   @override
   Widget build(final BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     final API.Player player = widget.player;
     return Container(
       color: Colors.grey[850],
@@ -35,15 +37,34 @@ class PlayerState extends State<PlayerTab> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Container(
-                  child: Text(player != null && player.nick != null ? player.nick : "Player Name", style: TextStyle(fontSize: 32.0)), // TODO: nick
+                  child: Text(player != null && player.nick != null ? player.nick : "Player Name", style: TextStyle(fontSize: 32.0)),
                   padding: EdgeInsets.all(16.0),
                   alignment: Alignment.topLeft,
                 ),
                 Container(
-                  child: Text(player != null && player.rank != null && player.rank.isNotEmpty ? player.rank : "No rank"),
+                  child: Text(player != null && player.rank != null && player.rank.isNotEmpty ? player.rank : "No rank", style: theme.textTheme.body1.copyWith(color: theme.textTheme.caption.color)),
                   padding: EdgeInsets.symmetric(horizontal: 16.0),
                   alignment: Alignment.topLeft,
                 ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      child: Icon(MdiIcons.headset, color: Colors.white),
+                      padding: EdgeInsets.all(16.0), // TODO: RHS should be 8.0
+                      alignment: Alignment.topLeft,
+                    ),
+                    Container(
+                      child: Icon(MdiIcons.heartPulse, color: Colors.red),
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      alignment: Alignment.topLeft,
+                    ),
+                    Container(
+                      child: Text("42"),
+                      padding: EdgeInsets.all(4.0),
+                      alignment: Alignment.topLeft,
+                    ),
+                  ],
+                )
                 // TODO: status (ingame/outgame)
                 // TODO: heart beat
                 // TODO: shot count
--- lib/team_tab.dart
+++ lib/team_tab.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:fixnum/fixnum.dart' show Int64;
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import 'api.dart' as API;
 import 'player_screen.dart';
@@ -67,6 +68,8 @@ class TeamList extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextStyle style = theme.textTheme.body1.copyWith(color: theme.textTheme.caption.color, fontSize: 12.0);
     return ListView.separated(
       padding: EdgeInsets.all(8.0),
       itemCount: players.length,
@@ -74,8 +77,35 @@ class TeamList extends StatelessWidget {
         final API.Player player = players[index];
         return ListTile(
           leading: CircleAvatar(child: Text(player.nick.substring(0, 1))),
-          title: Text(player.nick),
-          subtitle: Text(player.rank != null && player.rank.isNotEmpty ? player.rank : "No rank"),
+          title: Row(
+            children: <Widget>[
+              Text(player.nick),
+              Container(
+                child: Text(player.rank != null && player.rank.isNotEmpty ? player.rank : "No rank", style: style),
+                padding: EdgeInsets.only(left: 16.0),
+                alignment: Alignment.topLeft,
+              ),
+            ],
+          ),
+          subtitle: Row(
+            children: <Widget>[
+              Container(
+                child: Icon(index % 2 == 0 ? MdiIcons.headset : MdiIcons.headsetOff, color: Colors.white),
+                padding: EdgeInsets.all(0.0),
+                alignment: Alignment.topLeft,
+              ),
+              Container(
+                child: Icon(MdiIcons.heartPulse, color: Colors.red),
+                padding: EdgeInsets.only(left: 8.0),
+                alignment: Alignment.topLeft,
+              ),
+              Container(
+                child: Text("42"),
+                padding: EdgeInsets.only(left: 4.0),
+                alignment: Alignment.topLeft,
+              ),
+            ],
+          ),
           trailing: GestureDetector(
             child: Icon(Icons.info, color: Theme.of(context).disabledColor),
             onTap: () {
