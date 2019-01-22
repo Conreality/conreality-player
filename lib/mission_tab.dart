084b798c3cc45271de949424b83ca1fc8411e40e
Arto Bendiken <arto@conreality.org>
2019-01-22 16:08:08 +0200

    Refactored the mission tab structure.

--- lib/mission_tab.dart
+++ lib/mission_tab.dart
@@ -24,64 +24,92 @@ class MissionState extends State<MissionTab> {
   Widget build(final BuildContext context) {
     return Container(
       color: Colors.grey[850],
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          GameState(game: widget.info), // fixed
+          Expanded(child: GameDescription(game: widget.info)), // scrollable
+        ],
+      ),
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class GameState extends StatelessWidget {
+  final API.GameInformation game;
+
+  GameState({this.game});
+
+  @override
+  Widget build(final BuildContext context) {
+    return Container(
+      child: Text("Game not started yet", style: TextStyle(fontSize: 32.0)), // TODO: game state
+      padding: EdgeInsets.all(16.0),
+      alignment: Alignment.center,
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class GameDescription extends StatelessWidget {
+  final API.GameInformation game;
+
+  GameDescription({this.game});
+
+  @override
+  Widget build(final BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ExpansionTile(
+          title: Text("Mission", overflow: TextOverflow.ellipsis),
+          backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+          initiallyExpanded: true,
           children: <Widget>[
-            Container(
-              child: Text("Game not started yet", style: TextStyle(fontSize: 32.0)), // TODO: game state
-              padding: EdgeInsets.all(16.0),
-              alignment: Alignment.center,
-            ),
-            ExpansionTile(
-              title: Text("Mission", overflow: TextOverflow.ellipsis),
-              backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
-              initiallyExpanded: true,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: ListTile(
-                        title: Text(widget.info.title ?? ""),
-                        subtitle: Text(widget.info.mission ?? ""),
-                      ),
+                Expanded(
+                  child: ListTile(
+                    title: Text(game.title ?? ""),
+                    subtitle: Text(game.mission ?? ""),
+                  ),
+                ),
+                Column(
+                  children: (game.mission == null) ? <Widget>[] : <Widget>[
+                    GestureDetector(
+                      child: Icon(MdiIcons.playCircleOutline, color: Theme.of(context).disabledColor),
+                      onTap: () async => await say(game.mission),
                     ),
-                    Column(
-                      children: (widget.info.mission == null) ? <Widget>[] : <Widget>[
-                        GestureDetector(
-                          child: Icon(MdiIcons.playCircleOutline, color: Theme.of(context).disabledColor),
-                          onTap: () async => await say(widget.info.mission),
-                        ),
-                      ],
-                    ),
-                    Container(padding: EdgeInsets.only(right: 16.0)),
                   ],
                 ),
-                Container(padding: EdgeInsets.only(bottom: 16.0)),
+                Container(padding: EdgeInsets.only(right: 16.0)),
               ],
             ),
-            ExpansionTile(
-              title: Text("Theater", overflow: TextOverflow.ellipsis),
-              backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
-              initiallyExpanded: false,
-              children: <Widget>[
-                // TODO: origin, bounds, address, map?
-                Container(padding: EdgeInsets.only(bottom: 16.0)),
-              ],
-            ),
-            ExpansionTile(
-              title: Text("Rules", overflow: TextOverflow.ellipsis),
-              backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
-              initiallyExpanded: false,
-              children: <Widget>[
-                // TODO: list of rules
-                Container(padding: EdgeInsets.only(bottom: 16.0)),
-              ],
-            ),
+            Container(padding: EdgeInsets.only(bottom: 16.0)),
           ],
         ),
-      ),
+        ExpansionTile(
+          title: Text("Rules", overflow: TextOverflow.ellipsis),
+          backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+          initiallyExpanded: false,
+          children: <Widget>[
+            // TODO: list of rules
+            Container(padding: EdgeInsets.only(bottom: 16.0)),
+          ],
+        ),
+        ExpansionTile(
+          title: Text("Theater", overflow: TextOverflow.ellipsis),
+          backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+          initiallyExpanded: false,
+          children: <Widget>[
+            // TODO: origin, bounds, address, map?
+            Container(padding: EdgeInsets.only(bottom: 16.0)),
+          ],
+        ),
+      ],
     );
   }
 }
