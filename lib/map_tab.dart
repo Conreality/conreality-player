882da224b519d4127003b47ef8f0c14d102d38cb
Arto Bendiken <arto@conreality.org>
2019-01-26 05:31:51 +0200

    Added real-time player positions to the map.

--- lib/map_tab.dart
+++ lib/map_tab.dart
@@ -6,8 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart' show LatLng;
 
-import 'src/model.dart' show Game;
+import 'src/model.dart' show Game, Player;
 import 'src/session.dart' show GameSession;
+import 'src/spinner.dart' show Spinner;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -26,9 +27,53 @@ class MapTab extends StatefulWidget {
 
 class MapTabState extends State<MapTab> {
   final MapController _controller = MapController();
+  Timer _timer;
+  List<Marker> _markers;
 
-  void reload() {
-    setState(() {}); // TODO?
+  @override
+  void initState() {
+    super.initState();
+    reload();
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      reload();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  void reload() async {
+    final GameSession session = widget.session;
+    final Game game = session.game;
+
+    // Static markers for game landmarks:
+    final List<Marker> markers = <Marker>[
+      // The origin marker (using a home icon, for now):
+      Marker(
+        point: game.origin,
+        builder: (context) => Container(child: Icon(Icons.home, color: Colors.black)),
+      ),
+      // TODO: compass layer
+    ];
+
+    // Dynamic markers for all players:
+    final List<Player> players = await session.cache.listPlayers();
+    markers.addAll(players.map((final Player player) {
+      return !player.hasLocation ? null : Marker(
+        point: player.location,
+        builder: (context) => CircleAvatar( // TODO: PlayerAvatar
+          child: Text(player.nick.substring(0, 1)),
+          backgroundColor: session.cache.getColor(player.id),
+        ),
+      );
+    }).where((element) => element != null));
+
+    setState(() {
+      _markers = markers;
+    });
   }
 
   @override
@@ -47,30 +92,22 @@ class MapTabState extends State<MapTab> {
             'id': 'mapbox.outdoors',
           },
         ),
-        MarkerLayerOptions(
-          markers: <Marker>[
-            // The origin marker (using a home icon, for now):
-            Marker(
-              point: origin,
-              width: 40.0,
-              height: 40.0,
-              builder: (context) => Container(child: Icon(Icons.home, color: Colors.black)),
-            ),
-            // TODO: Compass
-          ],
-        ),
+        MarkerLayerOptions(markers: _markers ?? const <Marker>[]),
+/*
+        // FIXME: https://github.com/johnpryan/flutter_map/pull/213
         CircleLayerOptions(
           circles: <CircleMarker>[
             // The border for the game theater (a circular shape only, for now):
             CircleMarker(
               point: origin,
-              radius: game.radius, // FIXME: https://github.com/johnpryan/flutter_map/pull/213
+              radius: game.radius,
               color: Colors.blue.withOpacity(0.1),
               borderColor: Colors.red.withOpacity(1.0),
               borderStrokeWidth: 0.1, // FIXME: no effect?
             ),
           ],
         ),
+*/
       ],
     );
   }
